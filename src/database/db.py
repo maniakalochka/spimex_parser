96c2f7565f651344316ad9d995295d543bb2571e
Maniakalochka <prochorwolochoff@gmail.com>
2025-06-25 15:58:17 +0300

    Add SQLAlchemy database setup and base model classes

---
+++ src/database/db.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from core.config import settings
+from models.base import Base
+
+
+engine = create_engine(
+    url=settings.DB_URL,
+    echo=True,
+    pool_pre_ping=True,
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
+
+
+def init_db() -> None:
+    Base.metadata.create_all(bind=engine)
