7021eaa452186133be1c5d91f690a2f7a3c17c59
Maniakalochka <prochorwolochoff@gmail.com>
2025-06-28 23:51:02 +0300

    Remove unused import of Base from db.py

--- src/database/db.py
+++ src/database/db.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from core.config import settings
-from models.base import Base
 
 engine = create_engine(
     url=settings.DB_URL,
