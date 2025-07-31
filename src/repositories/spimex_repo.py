64c1c204890e866cb1d1102e48a57ace63176726
Maniakalochka <prochorwolochoff@gmail.com>
2025-07-31 09:29:41 +0300

    Remove unused imports from config.py and spimex_repo.py

--- src/repositories/spimex_repo.py
+++ src/repositories/spimex_repo.py
@@ -1,9 +1,7 @@
 #
 #
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.ext.asyncio import async_scoped_session
 from sqlalchemy import inspect
-from sqlalchemy.exc import SQLAlchemyError
 from models.spimex_trading_result import SpimexTradingResult
 
 
